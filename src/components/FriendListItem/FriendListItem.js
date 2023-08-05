088a358b14ad3f5a2da54a4c241abca317965cd0
Yurtov <123323348+Yurtov@users.noreply.github.com>
2023-08-05 14:02:55 +0300

    1

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -3,7 +3,7 @@ import { Item, Status, Name } from './FriendListItem.styled';
 export const FriendListItem = ({ friend: { avatar, name, isOnline} }) => {
   return (
     <Item>
-      <Status $status={`${isOnline}`}></Status>
+      <Status $status={isOnline}></Status>
       <img src={avatar} alt="User avatar" width="48" />
       <Name>{name}</Name>
     </Item>
